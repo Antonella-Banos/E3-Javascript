6ca82bb0200237da35d57d1125c8fd1b22b9ac52
Antonella <antonella.b95@hotmail.com>
2023-05-16 23:26:08 -0300

    renderización

--- E3-template-base/index.js
+++ E3-template-base/index.js
@@ -46,3 +46,85 @@ const pizzas = [
   },
 ];
 
+////////////////////////////////
+
+const pizzaContainer = document.querySelector(".pizza-container");
+const inputNumber = document.querySelector(".input-number");
+const errorContainer = document.querySelector(".error-container");
+
+
+
+
+const showPizza = (unaPizza) => {
+
+  pizzaContainer.innerHTML = "";
+
+  let div = document.createElement("div");
+  div.classList.add("pizza-container");
+
+  let h3 = document.createElement("h3");
+  h3.classList.add("pizza-name");
+  h3.innerText = unaPizza.nombre;
+
+  let img = document.createElement("img");
+  img.classList.add("pizza-img");
+  img.src = unaPizza.imagen;
+
+  let p = document.createElement("p");
+  p.classList.add("pizza-price");
+  p.textContent = "$" + unaPizza.precio;
+
+  pizzaContainer.appendChild(div);
+  div.appendChild(h3);
+  div.appendChild(img);
+  div.appendChild(p);
+
+}
+
+const errorMsg = () => {
+
+  pizzaContainer.innerHTML = "";
+  inputNumber.classList.add("error-message");
+  errorContainer.innerHTML = "";
+
+
+  const errorAlert = document.createElement("alert");
+  alert.classList.add("error-message");
+  alert.innerText = message;
+
+  errorContainer.appendChild(alert);
+  
+}
+
+
+
+const searchPizza = () => {
+
+  let numero = inputNumber.value;
+  inputNumber.classList.remove("error-message");
+  errorContainer.innerHTML = "";
+
+  if (numero.trim() === "") {
+    errorMsg("Debe ingresar un número");
+    return;
+  }
+  
+  for (let i = 0; i < pizzas.length; i++) {
+    const unaPizza = pizzas[i];
+    if (unaPizza.id === numero) {
+
+      showPizza(unaPizza);
+
+    }
+  }
+
+
+}
+
+
+init = () => {}
+
+init();
+
+
+
